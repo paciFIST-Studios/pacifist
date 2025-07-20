c5430f48c5e7c64839f425da90057770765a19ee
ellie <ellie@pacifist.studio>
2025-07-19 22:46:40 -0700

    adding tests

--- test/os_tests/os_utility.test.c
+++ test/os_tests/os_utility.test.c
@@ -5,7 +5,9 @@
 #include "../../src/os/os_utility.h"
 
 // stdlib
+#include <linux/limits.h>
 #include <stdio.h>
+#include <unistd.h>
 // framework
 // engine
 #include "../../src/core/define.h"
@@ -20,20 +22,19 @@ START_TEST(fn_file_size__is_defined) {
 }
 END_TEST
 
-START_TEST(fn_file_size__returns_minus_one__for_null_arg) {
+START_TEST(fn_file_size__returns_minus_one__for_null_path) {
     pf_set_error_suppressed();
     ck_assert(file_size(NULL, 0) == -1);
     pf_set_error_not_suppressed();
 }
 
-START_TEST(fn_file_size__returns_minus_one__for_bad_path_length) {
+START_TEST(fn_file_size__returns_minus_one__for_zero_path_length) {
     pf_set_error_suppressed();
     char * path = "";
     ck_assert(file_size(path, 0) == -1);
     pf_set_error_not_suppressed();
 }
 END_TEST
-
 
 START_TEST(fn_file_size__returns_minus_one__if_file_does_not_exist) {
     pf_set_error_suppressed();
@@ -46,9 +47,7 @@ START_TEST(fn_file_size__returns_minus_one__if_file_does_not_exist) {
 }
 END_TEST
 
-
-
-START_TEST(fn_file_size__writes_correct_error__for_null_arg) {
+START_TEST(fn_file_size__writes_correct_error__for_null_path) {
     // turn on error suppression, so it won't write to stdout
     pf_set_error_suppressed();
 
@@ -72,8 +71,7 @@ START_TEST(fn_file_size__writes_correct_error__for_null_arg) {
 }
 END_TEST
 
-
-START_TEST(fn_file_size__writes_correct_error__for_bad_path_length) {
+START_TEST(fn_file_size__writes_correct_error__for_zero_path_length) {
     // turn on error suppression, so it won't write to stdout
     pf_set_error_suppressed();
 
@@ -97,7 +95,6 @@ START_TEST(fn_file_size__writes_correct_error__for_bad_path_length) {
     ck_assert_int_eq(1, pstring_contains_char_sub(msg, expected, expected_length));
 }
 END_TEST
-
 
 START_TEST(fn_file_size__writes_correct_error__if_file_does_not_exist) {
     // turn on error suppression, so it won't write to stdout
@@ -127,15 +124,91 @@ START_TEST(fn_file_size__writes_correct_error__if_file_does_not_exist) {
 END_TEST
 
 
-
-
-
-
 // fn is_file ----------------------------------------------------------------------------------------------------------
 
 START_TEST(fn_is_file__is_defined) {
     int32_t (*fptr)(const char *, size_t const) = &is_file; 
     ck_assert_ptr_nonnull(fptr);
+}
+END_TEST
+
+START_TEST(fn_is_file__returns_minus_one__for_null_path) {
+    pf_set_error_suppressed();
+    ck_assert_int_eq(-1, is_file(NULL, 0));
+    pf_set_error_not_suppressed();
+}
+END_TEST
+
+START_TEST(fn_is_file__returns_minus_one__for_zero_path_length) {
+    pf_set_error_suppressed();
+    char * path = "";
+    ck_assert_int_eq(-1, is_file(path, 0));
+    pf_set_error_not_suppressed();
+}
+END_TEST
+
+START_TEST(fn_is_file__returns_zero__if_file_does_not_exist) {
+    char path[200];
+    for (size_t i = 0; i < 200; i++) { path[i] = 0; }
+    sprintf(path, "%s.get_wrekd", __FILE__);
+    size_t const len = pf_strlen(path);
+    ck_assert_int_eq(FALSE, is_file(path, len)); 
+}
+END_TEST
+
+START_TEST(fn_is_file__returns_one__if_file_does_exist) {
+    char const * path = __FILE__;
+    size_t const len = pf_strlen(path);
+    ck_assert_int_eq(TRUE, is_file(path, len)); 
+}
+END_TEST
+
+START_TEST(fn_is_file__writes_correct_error__for_null_path) {
+    // turn on error suppression, so it won't write to stdout
+    pf_set_error_suppressed();
+
+    // call the fn in a condition where it generates this error
+    is_file(NULL, 1);
+
+    // turn off error suppression, so we can get the error
+    pf_set_error_not_suppressed();
+    
+    // retrieve the error and put it in a pstring
+    char * error = pf_get_error();
+    size_t const error_length = pf_strlen(error);
+    PString_t const msg = { .string = error, .length = error_length };
+
+    // set up the expected result
+    char const * expected = "Invalid ptr to file path";  
+    size_t const expected_length = pf_strlen(expected);
+
+    // did we get the expected result?
+    ck_assert_int_eq(1, pstring_contains_char_sub(msg, expected, expected_length));
+}
+END_TEST
+
+START_TEST(fn_is_file__writes_correct_error__for_zero_path_length) {
+    // turn on error suppression, so it won't write to stdout
+    pf_set_error_suppressed();
+
+    // call the fn in a condition where it generates this error
+    char * path = "";
+    is_file(path, 0);
+
+    // turn off error suppression, so we can get the error
+    pf_set_error_not_suppressed();
+    
+    // retrieve the error and put it in a pstring
+    char * error = pf_get_error();
+    size_t const error_length = pf_strlen(error);
+    PString_t const msg = { .string = error, .length = error_length };
+
+    // set up the expected result
+    char const * expected = "Invalid file path length";  
+    size_t const expected_length = pf_strlen(expected);
+
+    // did we get the expected result?
+    ck_assert_int_eq(1, pstring_contains_char_sub(msg, expected, expected_length));
 }
 END_TEST
 
@@ -148,3 +221,83 @@ START_TEST(fn_is_directory__is_defined) {
 }
 END_TEST
 
+START_TEST(fn_is_directory__returns_minus_one__for_null_path) {
+    pf_set_error_suppressed();
+    ck_assert_int_eq(-1, is_directory(NULL, 0));
+    pf_set_error_not_suppressed();
+}
+END_TEST
+
+START_TEST(fn_if_directory__returns_minus_one__for_zero_path_length) {
+    pf_set_error_suppressed();
+    char * path = "";
+    ck_assert_int_eq(-1, is_directory(path, 0));
+    pf_set_error_not_suppressed();
+}
+END_TEST
+
+START_TEST(fn_is_directory__returns_zero__if_path_is_not_a_directory) {
+    char* path = __FILE__;
+    size_t const len = pf_strlen(path);
+    ck_assert_int_eq(FALSE, is_directory(path, len));
+}
+END_TEST
+
+START_TEST(fn_is_directory__returns_one__if_path_is_a_directory) {
+    char path[PATH_MAX];
+    for (size_t i = 0; i < PATH_MAX; i++) { path[i] = 0; }
+    ck_assert_ptr_nonnull(getcwd(path, PATH_MAX)); 
+    size_t const len = pf_strlen(path);
+    ck_assert_int_eq(TRUE, is_directory(path, len)); 
+}
+END_TEST
+
+START_TEST(fn_is_directory__writes_correct_error__for_null_path) {
+    // turn on error suppression, so it won't write to stdout
+    pf_set_error_suppressed();
+
+    // call the fn in a condition where it generates this error
+    is_directory(NULL, 1);
+
+    // turn off error suppression, so we can get the error
+    pf_set_error_not_suppressed();
+    
+    // retrieve the error and put it in a pstring
+    char * error = pf_get_error();
+    size_t const error_length = pf_strlen(error);
+    PString_t const msg = { .string = error, .length = error_length };
+
+    // set up the expected result
+    char const * expected = "Invalid ptr to file path";  
+    size_t const expected_length = pf_strlen(expected);
+
+    // did we get the expected result?
+    ck_assert_int_eq(1, pstring_contains_char_sub(msg, expected, expected_length));
+}
+END_TEST
+
+START_TEST(fn_is_directory__writes_correct_error__for_zero_path_length) {
+    // turn on error suppression, so it won't write to stdout
+    pf_set_error_suppressed();
+
+    // call the fn in a condition where it generates this error
+    char * path = "";
+    is_directory(path, 0);
+
+    // turn off error suppression, so we can get the error
+    pf_set_error_not_suppressed();
+    
+    // retrieve the error and put it in a pstring
+    char * error = pf_get_error();
+    size_t const error_length = pf_strlen(error);
+    PString_t const msg = { .string = error, .length = error_length };
+
+    // set up the expected result
+    char const * expected = "Invalid file path length";  
+    size_t const expected_length = pf_strlen(expected);
+
+    // did we get the expected result?
+    ck_assert_int_eq(1, pstring_contains_char_sub(msg, expected, expected_length));
+    
+}
+END_TEST
