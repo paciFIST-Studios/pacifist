b1455ad0e0ef157b8b1e514df89ee0859f499596
ellie <ellie@pacifist.studio>
2025-07-24 23:06:15 -0700

    made a bunch of things const

--- test/string_tests/PString.test.c
+++ test/string_tests/PString.test.c
@@ -9,11 +9,12 @@
 // engine
 #include "../../src/core/define.h"
 #include "../../src/core/error.h"
+#include "../../src/memory/hash_table/HashTable.h"
 
 // struct PString_t ------------------------------------------------------------------------------------------
 
 START_TEST(struct_PString_t__is_defined) {
-    PString_t pstr = { .string = NULL, .length = 0 };
+    PString_t const pstr = { .string = NULL, .length = 0 };
     ck_assert_ptr_nonnull(&pstr);
     // also checks members
     ck_assert_ptr_null(pstr.string);
@@ -35,8 +36,8 @@ START_TEST(fn_pstring_contains_pstr_sub__is_defined) {
 END_TEST
 
 START_TEST(fn_pstring_contains_pstr_sub__returns_false__if_substring_is_longer_that_first_string) {
-    PString_t pstr1 = { .string = "a", .length = 1 };
-    PString_t pstr2 = { .string = "ab", .length = 2 };
+    PString_t const pstr1 = { .string = "a", .length = 1 };
+    PString_t const pstr2 = { .string = "ab", .length = 2 };
     PF_SUPPRESS_ERRORS
     ck_assert_int_eq(FALSE, pstring_contains_pstr_sub(pstr1, pstr2));
     PF_UNSUPPRESS_ERRORS
@@ -44,8 +45,8 @@ START_TEST(fn_pstring_contains_pstr_sub__returns_false__if_substring_is_longer_t
 END_TEST
 
 START_TEST(fn_pstring_contains_pstr_sub__logs_correct_error__if_substring_is_longer_than_first_string) {
-    PString_t pstr1 = { .string = "a", .length = 1 }; 
-    PString_t pstr2 = { .string = "ab", .length = 2 };
+    PString_t const pstr1 = { .string = "a", .length = 1 }; 
+    PString_t const pstr2 = { .string = "ab", .length = 2 };
     pf_clear_error();
     
     PF_SUPPRESS_ERRORS
@@ -56,15 +57,15 @@ START_TEST(fn_pstring_contains_pstr_sub__logs_correct_error__if_substring_is_lon
     size_t const expected_length = pf_strlen(expected_error);
     char * found_error = pf_get_error();
 
-    PString_t pstr_found_error = { .string = found_error, .length = pf_strlen(found_error) };
+    PString_t const pstr_found_error = { .string = found_error, .length = pf_strlen(found_error) };
     ck_assert_int_eq(TRUE, pstring_contains_char_sub(pstr_found_error, expected_error, expected_length));
 }
 END_TEST
 
 
 START_TEST(fn_pstring_contains_pstr_sub__can_find_a_substring) {
-    PString_t a = { .string = "ab", .length = 2 };
-    PString_t b = { .string =  "a", .length = 1 };
+    PString_t const a = { .string = "ab", .length = 2 };
+    PString_t const b = { .string =  "a", .length = 1 };
     
     //pstring_contains_substring(a, b);  
     ck_assert_int_eq(pstring_contains_pstr_sub(a, b), 1);  
@@ -74,8 +75,8 @@ END_TEST
 START_TEST(fn_pstring_contains_pstr_sub__stress_testing) {
     char * test_string_a = "gold old ld d";
     char * test_substring_a = "gold";
-    PString_t ta = { .string = test_string_a, .length = strlen(test_string_a) };
-    PString_t tsa = { .string = test_substring_a, .length = strlen(test_substring_a) };
+    PString_t const ta = { .string = test_string_a, .length = strlen(test_string_a) };
+    PString_t const tsa = { .string = test_substring_a, .length = strlen(test_substring_a) };
 
     ck_assert_int_eq(1, pstring_contains_pstr_sub(ta, tsa));
     
@@ -84,11 +85,11 @@ START_TEST(fn_pstring_contains_pstr_sub__stress_testing) {
     char * test_substring2 = " in the ";
     char * test_substring3 = "moil";
     char * test_substring4 = "12moil345";
-    PString_t a = { .string = test_string, .length = strlen(test_string) };
-    PString_t b = { .string = test_substring1, .length = strlen(test_substring1) };
-    PString_t c = { .string = test_substring2, .length = strlen(test_substring2) };
-    PString_t d = { .string = test_substring3, .length = strlen(test_substring3) };
-    PString_t e = { .string = test_substring4, .length = strlen(test_substring4) };
+    PString_t const a = { .string = test_string, .length = strlen(test_string) };
+    PString_t const b = { .string = test_substring1, .length = strlen(test_substring1) };
+    PString_t const c = { .string = test_substring2, .length = strlen(test_substring2) };
+    PString_t const d = { .string = test_substring3, .length = strlen(test_substring3) };
+    PString_t const e = { .string = test_substring4, .length = strlen(test_substring4) };
 
     // matches substring which is part of a word
     ck_assert_int_eq(1, pstring_contains_pstr_sub(a, b));
@@ -121,9 +122,9 @@ START_TEST(fn_pstring_slice__is_defined) {
 END_TEST
 
 START_TEST(fn_pstirng_slice__returns_null_result__of_correct_form) {
-    PString_t pstr = { .string = NULL, .length = 0 };
+    PString_t const pstr = { .string = NULL, .length = 0 };
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 0, 1);
+    PString_t const result = pstring_slice(pstr, 0, 1);
     PF_UNSUPPRESS_ERRORS
     ck_assert_ptr_nonnull(&result);
     ck_assert_int_eq(result.length, 0);
@@ -132,9 +133,9 @@ START_TEST(fn_pstirng_slice__returns_null_result__of_correct_form) {
 END_TEST
 
 START_TEST(fn_pstring_slice__returns_null_result__for_pstr_arg_with_null_string) {
-    PString_t pstr = { .string = NULL, .length = 1 };
+    PString_t const pstr = { .string = NULL, .length = 1 };
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 0, 1);
+    PString_t const result = pstring_slice(pstr, 0, 1);
     PF_UNSUPPRESS_ERRORS
     ck_assert_ptr_null(result.string);
     ck_assert_int_eq(result.length, 0);
@@ -145,7 +146,7 @@ START_TEST(fn_pstring_slice__sets_correct_error_message__for_pstring_arg_with_nu
     PString_t const pstr = { .string = NULL, .length = 1 };
     pf_clear_error();
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 0, 1);
+    PString_t const result = pstring_slice(pstr, 0, 1);
     PF_UNSUPPRESS_ERRORS
 
     char const * error_message = pf_get_error();
@@ -159,9 +160,9 @@ START_TEST(fn_pstring_slice__sets_correct_error_message__for_pstring_arg_with_nu
 END_TEST
 
 START_TEST(fn_pstring_slice__returns_null_result__for_pstr_arg_with_zero_length) {
-    PString_t pstr = { .string = "test", .length = 0 };
+    PString_t const pstr = { .string = "test", .length = 0 };
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 0, 1);
+    PString_t const result = pstring_slice(pstr, 0, 1);
     PF_UNSUPPRESS_ERRORS
     ck_assert_ptr_null(result.string);
     ck_assert_int_eq(result.length, 0);
@@ -169,10 +170,10 @@ START_TEST(fn_pstring_slice__returns_null_result__for_pstr_arg_with_zero_length)
 END_TEST
 
 START_TEST(fn_pstring_slive__sets_correct_error_message__for_pstr_arg_with_zero_length) {
-    PString_t pstr = { .string = "test", .length = 0 };
+    PString_t const pstr = { .string = "test", .length = 0 };
     pf_clear_error();
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 0, 1);
+    PString_t const result = pstring_slice(pstr, 0, 1);
     PF_UNSUPPRESS_ERRORS
 
     char const * error_message = pf_get_error();
@@ -186,18 +187,18 @@ START_TEST(fn_pstring_slive__sets_correct_error_message__for_pstr_arg_with_zero_
 END_TEST
 
 START_TEST(fn_pstring_slice__returns_null_result__for_request_of_zero_length_slice) {
-    PString_t pstr = { .string = "words", .length = 5 };
+    PString_t const pstr = { .string = "words", .length = 5 };
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 2, 2);
+    PString_t const result = pstring_slice(pstr, 2, 2);
     PF_UNSUPPRESS_ERRORS
     ck_assert_ptr_null(result.string);
     ck_assert_int_eq(result.length, 0);
 }
 
 START_TEST(fn_pstring_slice__sets_correct_error_message__for_request_of_zero_length_slice) {
-    PString_t pstr = { .string = "words", .length = 5 };
+    PString_t const pstr = { .string = "words", .length = 5 };
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 2, 2);
+    PString_t const result = pstring_slice(pstr, 2, 2);
     PF_UNSUPPRESS_ERRORS
 
     char const * error_message = pf_get_error();
@@ -211,18 +212,18 @@ START_TEST(fn_pstring_slice__sets_correct_error_message__for_request_of_zero_len
 END_TEST
 
 START_TEST(fn_pstring_slice__returns_null_result__for_out_of_bounds_begin_idx) {
-    PString_t pstr = { .string = "a", .length = 1 };
+    PString_t const pstr = { .string = "a", .length = 1 };
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 2, 1);
+    PString_t const result = pstring_slice(pstr, 2, 1);
     PF_UNSUPPRESS_ERRORS
     ck_assert_ptr_null(result.string);
     ck_assert_int_eq(result.length, 0);
 }
 
 START_TEST(fn_pstring_slice__sets_correct_error_message__for_out_of_bounds_begin_idx) {
-    PString_t pstr = { .string = "a", .length = 1 };
+    PString_t const pstr = { .string = "a", .length = 1 };
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 2, 1);
+    PString_t const result = pstring_slice(pstr, 2, 1);
     PF_UNSUPPRESS_ERRORS
 
     char const * error_message = pf_get_error();
@@ -236,10 +237,10 @@ START_TEST(fn_pstring_slice__sets_correct_error_message__for_out_of_bounds_begin
 END_TEST
 
 START_TEST(fn_pstring_slice__returns_null_result__for_out_of_bounds_end_idx) {
-    PString_t pstr = { .string = "a", .length = 1 };
+    PString_t const pstr = { .string = "a", .length = 1 };
     pf_clear_error();
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 0, 2);
+    PString_t const result = pstring_slice(pstr, 0, 2);
     PF_UNSUPPRESS_ERRORS
     ck_assert_ptr_null(result.string);
     ck_assert_int_eq(result.length, 0);
@@ -250,7 +251,7 @@ START_TEST(fn_pstring_slice__sets_correct_error_message__for_out_of_bounds_end_i
     PString_t const pstr = { .string = "a", .length = 1 };
     pf_clear_error();
     PF_SUPPRESS_ERRORS
-    PString_t result = pstring_slice(pstr, 0, 2);
+    PString_t const result = pstring_slice(pstr, 0, 2);
     PF_UNSUPPRESS_ERRORS
 
     char const * error_message = pf_get_error();
@@ -270,7 +271,7 @@ START_TEST(fn_pstring_slice__returns_non_owning_pstring__for_valid_slice) {
 
     int32_t const begin = 10;
     int32_t const end = 14;
-    PString_t result = pstring_slice(pstr, begin, end);
+    PString_t const result = pstring_slice(pstr, begin, end);
     ck_assert_ptr_nonnull(result.string);
     ck_assert_int_eq(result.length, end - begin);
 
@@ -286,7 +287,7 @@ START_TEST(fn_pstring_slice__returns_slice__when_using_negative_indicies) {
     int32_t const begin = -12;
     int32_t const end = PSTR_LAST;
     size_t const len = abs(end - begin);
-    PString_t result = pstring_slice(pstr, begin, end);
+    PString_t const result = pstring_slice(pstr, begin, end);
     ck_assert_ptr_nonnull(result.string);
     ck_assert_int_eq(result.length, len);
 }
@@ -300,7 +301,7 @@ START_TEST(fn_pstring_slice__returns_slice__when_using_positive_then_negative_in
     int32_t const begin = 12;
     int32_t const end = PSTR_LAST;
     size_t const len = test_length - begin;
-    PString_t result = pstring_slice(pstr, begin, end);
+    PString_t const result = pstring_slice(pstr, begin, end);
     ck_assert_ptr_nonnull(result.string);
     ck_assert_int_eq(result.length, len);
 }
@@ -311,16 +312,15 @@ START_TEST(fn_pstring_slice__feels_okay_in_casual_usage) {
     size_t const test_length = pf_strlen(test_string);
     PString_t const pstr = { .string = (char*)test_string, test_length };
 
-    PString_t result = pstring_slice(pstr, 14, PSTR_LAST);
+    PString_t const result = pstring_slice(pstr, 14, PSTR_LAST);
 
     char * fireball = malloc(9);
     for (size_t i = 0; i < 9; i++) { fireball[i] = 0; }
-    
+
     strncpy(fireball, result.string, 9);
 
-    int32_t bContainsSubstring = pstring_contains_char_sub(result, fireball, 9);
-    ck_assert_int_eq(bContainsSubstring, TRUE);
-    
+    ck_assert_int_eq(TRUE, pstring_contains_char_sub(result, fireball, 9));
+
     free(fireball);
 }
 
