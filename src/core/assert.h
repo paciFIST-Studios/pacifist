cce797c0238feb9442521a9b18891263db439e52
ellie <ellie@pacifist.studio>
2025-08-02 15:36:51 -0700

    updated assert to use the pf_ naming convention

--- src/core/assert.h
+++ src/core/assert.h
@@ -9,18 +9,18 @@
 #define BUILD_SETTING_ALLOW_ASSERTS 1
 
 #ifdef BUILD_SETTING_ALLOW_ASSERTS
-inline void do_assertion(char const * fn, char const * file, int line) {
+static inline void pf_do_assertion(char const * fn, char const * file, int const line) {
     fprintf(stderr, "ASSERTION FAILED! fn: %s, file: %s, line: %d", fn, file, line);
     // force a crash using null deref
     *(int*)0 = 0;
 }
-#define ASSERT(expression) do { if(!expression){ do_assertion(__func__, __FILE__, __LINE__); } } while(0);
+#define PF_ASSERT(expression) do { if(!expression){ pf_do_assertion(__func__, __FILE__, __LINE__); } } while(0);
 #else
-#define ASSERT(expression);
+#define PF_ASSERT(expression);
 #endif
 
 #ifdef BUILD_SETTING_ALLOW_ASSERTS
-static inline void do_assertion_message(char const * message, char const * fn, char const * file, int line) {
+static inline void pf_do_assertion_message(char const * message, char const * fn, char const * file, int line) {
     fprintf(stderr, "ASSERTION FAILED!  %s  fn: %s, file: %s, line: %d", message, fn, file, line);
     // force a crash using null deref
     *(int*)0 = 0;
@@ -28,7 +28,7 @@ static inline void do_assertion_message(char const * message, char const * fn, c
 
 // Asserts that the condition is true.  If it is not, this message is printed to stderr, along with the function, file, and line 
 #define ASSERT_MESSAGE(expression, message) \
-do { if(!expression){ do_assertion_message(message, __func__, __FILE__, __LINE__); } } while(0);
+do { if(!expression){ pf_do_assertion_message(message, __func__, __FILE__, __LINE__); } } while(0);
 #else
 
 #endif
