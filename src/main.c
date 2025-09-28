50a70013380118dcdd8f80bffd3a4b2e058c81ee
ellie <ellie@pacifist.studio>
2025-09-27 22:42:29 -0700

    some changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
 // engine
-#include "engine/entry_point/PFEngineEntryPoint.h"
+#include "engine/entry_point/pf_engine_entry_point.h"
 // game
 
 
