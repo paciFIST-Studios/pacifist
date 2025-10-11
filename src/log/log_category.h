85d869bacaaf1506f9660f2f64d204737a1b38c6
ellie <ellie@pacifist.studio>
2025-10-11 10:43:50 -0700

    added log category

--- src/log/log_category.h
+++ src/log/log_category.h
@@ -8,6 +8,9 @@
 #include <SDL3/SDL_log.h>
 // engine
 
+/**
+ * These are logging categories, used to indicate what kind of error/log message is being issued
+ */
 typedef enum PFLogCategory_t {
     PF_APPLICATION = SDL_LOG_CATEGORY_CUSTOM, /* this one should always come first, even if it's not alphabetical */
     PF_ALLOCATOR,
@@ -18,12 +21,12 @@ typedef enum PFLogCategory_t {
     PF_GAMEPLAY,
     PF_INITIALIZATION,
     PF_OS,
+    PF_PARSE,
     PF_RENDER,
     PF_RESOURCE,
     PF_SERIALIZATION,
     PF_STRING,
     PF_TEXTURE,
 } PFLogCategory_t;
-
 
 #endif //LOG_CATEGORY_H
